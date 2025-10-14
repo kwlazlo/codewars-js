c5658ff5668e7987783ca85124d383a861eb8a54
kwlazlo <krzysztofwlazlo@icloud.com>
2025-10-14 16:27:42 +0200

    another day

---
+++ fundamentals/dontgivemefive.js
@@ -0,0 +1,13 @@
+function dontGiveMeFive(start, end) {
+  let count = 0;
+
+  for (let i = start; i <= end; i++) {
+    if (!String(i).includes("5")) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(dontGiveMeFive(3, 12));
