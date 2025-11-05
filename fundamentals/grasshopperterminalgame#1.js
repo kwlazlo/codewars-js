630bd255cc1d069cdbeee54083b1c4802cfdef56
kwlazlo <krzysztofwlazlo@icloud.com>
2025-11-05 09:01:37 +0100

    another day

---
+++ fundamentals/grasshopperterminalgame#1.js
@@ -0,0 +1,18 @@
+class Hero {
+  constructor(
+    name = "Hero",
+    position = "00",
+    health = 100,
+    damage = 5,
+    experience = 0
+  ) {
+    this.name = name;
+    this.position = position;
+    this.health = health;
+    this.damage = damage;
+    this.experience = experience;
+  }
+}
+
+const hero = new Hero();
+console.log(hero);
